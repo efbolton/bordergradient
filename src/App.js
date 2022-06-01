15990255ca5c46fbe5d21665b0629806e924cb37
efbolton <EFBOLTON@HOTMAIL.COM>
2022-06-01 12:46:05 -0400

    20220601

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+//https://v2.tailwindcss.com/docs/guides/create-react-app
+//https://github.com/facebook/create-react-app/issues/11777
+//https://hackernoon.com/create-a-gradient-border-blog-postcard-using-tailwind-css-and-nextjs-a-how-to-guide
+//https://tailwindcss.com/docs/background-image
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col text-white items-center bg-[#111827] min-h-screen ">
+      content
+      {/* 1st div of 2 */}
+      <div className="flex bg-[#111827] w-full items-center ">
+        <div className="flex items-center place-content-center w-full h-64 bg-[url('bckg.jpg')] bg-center bg-contain bg-no-repeat">
+          more content
+        </div>
+      </div>
+      {/* 
+      2nd div of 2
+      cyan faded border gradient 
+      eg https://tailwindui.com/pricing
+      */}
+      <div className="rounded-lg w-52 h-64 mx-auto mt-10 bg-gradient-to-b p-[1px] from-[cyan] to-[black]">
+        <div className="flex flex-col justify-between items-center h-full bg-black text-white rounded-lg p-4">
+          yup even more content
+        </div>
+      </div>
     </div>
   );
 }
